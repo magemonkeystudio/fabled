e20111ea6594845f734faf5c87b24088e7eff6e5
Sentropic <60368610+Sentropic@users.noreply.github.com>
2020-11-25 16:50:48 -0600

    Fixed <1.13 WorldGuard compatibility

--- src/main/java/com/sucy/skill/hook/WorldGuardHook.java
+++ src/main/java/com/sucy/skill/hook/WorldGuardHook.java
@@ -1,8 +1,8 @@
 package com.sucy.skill.hook;
 
 import com.google.common.collect.ImmutableList;
-import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.managers.RegionManager;
@@ -10,6 +10,7 @@ import com.sucy.skill.SkillAPI;
 import org.bukkit.Location;
 import org.bukkit.World;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.List;
 
@@ -20,6 +21,10 @@ import java.util.List;
 public class WorldGuardHook {
 
     private static Method regionMethod;
+
+    private static Class<?> vectorClass;
+    private static Constructor<?> vectorConstructor;
+    private static Method applicableRegionsMethod;
 
     /**
      * Fetches the list of region IDs applicable to a given location
@@ -33,21 +38,26 @@ public class WorldGuardHook {
                     .getPlatform()
                     .getRegionContainer()
                     .get(BukkitAdapter.adapt(loc.getWorld()))
-                    .getApplicableRegionsIDs(asVector(loc));
+                    .getApplicableRegionsIDs(BlockVector3.at(loc.getX(), loc.getY(),loc.getZ()));
         } catch (NoClassDefFoundError ex) {
             try {
                 final WorldGuardPlugin plugin = SkillAPI.getPlugin(WorldGuardPlugin.class);
-                return ((RegionManager) getRegionMethod().invoke(plugin, loc.getWorld()))
-                        .getApplicableRegionsIDs(asVector(loc));
+                return (List<String>) getApplicableRegionsMethod().invoke((getRegionMethod().invoke(plugin, loc.getWorld())), vectorConstructor.newInstance(loc.getX(), loc.getY(),loc.getZ()));
             } catch (final Exception e) {
                 // Cannot handle world guard
+                e.printStackTrace();
                 return ImmutableList.of();
             }
         }
     }
 
-    private static BlockVector3 asVector(final Location location) {
-        return BlockVector3.at(location.getX(), location.getY(),location.getZ());
+    private static Method getApplicableRegionsMethod() throws Exception {
+        if (applicableRegionsMethod == null) {
+            vectorClass = Class.forName("com.sk89q.worldedit.Vector");
+            vectorConstructor = vectorClass.getConstructor(double.class, double.class, double.class);
+            applicableRegionsMethod = RegionManager.class.getMethod("getApplicableRegionsIDs", vectorClass);
+        }
+        return applicableRegionsMethod;
     }
 
     private static Method getRegionMethod() throws Exception {
