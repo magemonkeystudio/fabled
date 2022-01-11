e14000bccdc91def719a58278b62eff0ff7a565b
Sentropic <sentropic.sv@gmail.com>
2022-01-11 16:28:10 -0600

    Fix BlockMechanic inaccuracy

--- src/main/java/com/sucy/skill/dynamic/mechanic/BlockMechanic.java
+++ src/main/java/com/sucy/skill/dynamic/mechanic/BlockMechanic.java
@@ -193,7 +193,7 @@ public class BlockMechanic extends MechanicComponent {
                 for (double i = x - width; i <= x + width + 0.01; i++) {
                     for (double j = y - height; j <= y + height + 0.01; j++) {
                         for (double k = z - depth; k <= z + depth + 0.01; k++) {
-                            Block b = w.getBlockAt((int) (facingZ ? i : k), (int) j, (int) (facingZ ? k : i));
+                            Block b = w.getBlockAt((int) Math.floor(facingZ ? i : k), (int) Math.floor(j), (int) Math.floor(facingZ ? k : i));
                             if ((!solid || b.getType().isSolid())
                                     && (!air || b.getType() == Material.AIR)
                                     && !SkillAPI.getSettings().getFilteredBlocks().contains(b.getType())) {
