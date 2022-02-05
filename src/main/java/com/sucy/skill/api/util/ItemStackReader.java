06376d712aa75d107c0a0eb385f6cf729017efcd
Sentropic <sentropic.sv@gmail.com>
2022-02-05 16:44:27 -0600

    Add extra compatibility with old editors' item options

--- src/main/java/com/sucy/skill/api/util/ItemStackReader.java
+++ src/main/java/com/sucy/skill/api/util/ItemStackReader.java
@@ -19,9 +19,9 @@ import java.util.Objects;
 public final class ItemStackReader {
     private static final String MATERIAL   = "material";
     private static final String AMOUNT     = "amount";
-    private static final String DURABILITY = "data"; // Wrong name for retrocompatibility
+    private static final String DURABILITY = "durability";
     private static final String UNBREAKABLE = "unbreakable";
-    private static final String CMD        = "byte"; // Wrong name for retrocompatibility
+    private static final String CMD        = "cmd";
     private static final String HIDE_FLAGS = "hide-flags";
     private static final String CUSTOM     = "custom";
     private static final String NAME       = "name";
@@ -33,18 +33,55 @@ public final class ItemStackReader {
     private static final String POTION_DURATION = "potion_duration";
     private static final String ARMOR_COLOR = "armor_color";
 
+    // Retrocompatibility
+
+    private static final String DATA = "data"; // Previously used instead of 'durability'
+    private static final String BYTE = "byte"; // Previously used instead of 'cmd'
+    private static final String ITEM = "item"; // Previously used instead of 'material' in ItemProjectileMechanic
+    private static final String ITEM_DATA = "item-data"; // Previously used instead of 'cmd' in ItemProjectileMechanic
+
     private ItemStackReader() { }
 
+    public static Material readMaterial(Settings settings) {
+        String string = null;
+        if (settings.has(MATERIAL)) {
+            string = settings.getString(MATERIAL);
+        } else if (settings.has(ITEM)) {
+            string = settings.getString(ITEM);
+        }
+
+        try { return Material.valueOf(string.toUpperCase().replace(" ", "_")); }
+        catch (NullPointerException | IllegalArgumentException e) { return Material.ARROW; }
+    }
+
+    public static int readDurability(Settings settings) {
+        if (settings.has(DURABILITY)) {
+            return settings.getInt(DURABILITY);
+        } else {
+            return settings.getInt(DATA, 0);
+        }
+    }
+
+    public static int readCustomModelData(Settings settings) {
+        if (settings.has(CMD)) {
+            return settings.getInt(CMD);
+        } else if (settings.has(BYTE)) {
+            return settings.getInt(BYTE);
+        } else {
+            return settings.getInt(ITEM_DATA, 0);
+        }
+    }
+
     public static ItemStack read(Settings settings) {
-        ItemStack item = new ItemStack(Material.valueOf(settings.getString(MATERIAL, "arrow").toUpperCase().replace(" ", "_")), settings.getInt(AMOUNT, 1));
+        ItemStack item = new ItemStack(readMaterial(settings), settings.getInt(AMOUNT, 1));
         ItemMeta meta = Objects.requireNonNull(item.getItemMeta());
 
         if (meta instanceof Damageable) {
             Damageable damageable = (Damageable) meta;
-            damageable.setDamage(settings.getInt(DURABILITY, 0));
+            damageable.setDamage(readDurability(settings));
             damageable.setUnbreakable(settings.getBool(UNBREAKABLE, false));
         }
-        meta.setCustomModelData(settings.getInt(CMD, 0));
+        meta.setCustomModelData(readCustomModelData(settings));
 
         for (String hideFlag : settings.getStringList(HIDE_FLAGS)) {
             try {
