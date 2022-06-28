5f3b625ace9eee40f160daabfe9619c62ea7131e
Travja <the.only.t.craft@gmail.com>
2022-06-28 00:42:27 -0600

    Fix #325. Waterlogged block are actually checked now

--- src/main/java/com/sucy/skill/dynamic/condition/WaterCondition.java
+++ src/main/java/com/sucy/skill/dynamic/condition/WaterCondition.java
@@ -1,21 +1,21 @@
 /**
  * SkillAPI
  * com.sucy.skill.dynamic.condition.WaterCondition
- *
+ * <p>
  * The MIT License (MIT)
- *
+ * <p>
  * Copyright (c) 2014 Steven Sucy
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software") to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -40,20 +40,21 @@ public class WaterCondition extends ConditionComponent {
 
     @Override
     boolean test(final LivingEntity caster, final int level, final LivingEntity target) {
-        final boolean out = settings.getString(STATE).equalsIgnoreCase("out of water");
+        final boolean  in    = settings.getString(STATE).equalsIgnoreCase("in water");
         final Material block = target.getLocation().getBlock().getType();
 
-        final Block b =  target.getLocation().getBlock();
-        boolean isWaterLogged = false;
+        final Block b             = target.getLocation().getBlock();
+        boolean     isWaterLogged = false;
 
-        if(VersionManager.isVersionAtLeast(11300)) {
-            if(b.getBlockData() instanceof Waterlogged){
+        if (VersionManager.isVersionAtLeast(11300)) {
+            if (b.getBlockData() instanceof Waterlogged) {
                 Waterlogged wl = (Waterlogged) b.getBlockData();
                 isWaterLogged = wl.isWaterlogged();
             }
         }
 
-        return out != (block.name().contains("WATER")) || (isWaterLogged);
+        boolean inWater = block.name().contains("WATER") || isWaterLogged;
+        return in == inWater;
     }
 
     @Override
