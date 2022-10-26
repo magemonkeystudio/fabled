bf483b24fe442938e114bcc1c0e70c1b1a466927
Travja <the.only.t.craft@gmail.com>
2022-10-25 18:12:46 -0600

    Update workflows for Maven Central

--- .github/notify_published.py
+++ .github/notify_published.py
@@ -4,23 +4,26 @@ import re
 import sys
 
 def get_info():
-	headers = {'User-Agent': 'PostmanRuntime/7.29.0', 'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate, br', 'Connection': 'keep-alive'}
-	r = requests.get('https://github.com/promcteam/promccore/packages/1203744', headers=headers)
+    with open('log.txt', 'r') as file:
+        content = file.read()
+        data = re.findall(
+            r'Uploaded to ossrh: (https:\/\/s01\.oss\.sonatype\.org\/.*?\/com\/promcteam\/(.*?)\/(.*?)\/(.*?)\.jar)( .*)?$',
+            content, re.MULTILINE)
+        version = data[0][3]
+        id = data[0][1]
+        url = data[0][0]
+        return version, id, url
 
-	if r.status_code == 200:
-		version = re.findall('&lt;version&gt;((.|\w|\n)*?)&lt;\/version&gt;', str(r.content))[0][0]
-		id = re.findall('&lt;artifactId&gt;((.|\w|\n)*?)&lt;\/artifactId&gt;', str(r.content))[0][0]
-		return version, id
 
-version, name = get_info()
+version, name, url = get_info()
 embed = {
-	'author': {
-		'name': 'New Build Available!',
-		'url': 'https://github.com/promcteam/' + name
-	},
-	'title': name + '-' + version,
-	'url': 'https://github.com/promcteam/promccore/packages/1203744',
-	'color': 5341129
+    'author': {
+        'name': 'New Build Available!',
+        'url': 'https://github.com/promcteam/' + name
+    },
+    'title': version,
+    'url': url,
+    'color': 5341129
 }
 
 requests.post(sys.argv[1],
