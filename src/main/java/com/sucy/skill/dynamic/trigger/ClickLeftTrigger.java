1b7763100c884877ee05e54fd34add1aed3ebd17
Travja <the.only.t.craft@gmail.com>
2022-12-17 00:44:02 -0700

    Make left/right click triggers only trigger on main hand

--- src/main/java/com/sucy/skill/dynamic/trigger/ClickLeftTrigger.java
+++ src/main/java/com/sucy/skill/dynamic/trigger/ClickLeftTrigger.java
@@ -3,6 +3,7 @@ package com.sucy.skill.dynamic.trigger;
 import com.sucy.skill.api.Settings;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 public class ClickLeftTrigger extends ClickTrigger {
 
@@ -19,8 +20,8 @@ public class ClickLeftTrigger extends ClickTrigger {
      */
     @Override
     public boolean shouldTrigger(PlayerInteractEvent event, int level, Settings settings) {
-
-        if (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) {
+        if ((event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK)
+                && event.getHand() == EquipmentSlot.HAND) {
             return settings.getString("crouch").equalsIgnoreCase("both") ||
                     event.getPlayer().isSneaking() != settings.getString("crouch").equalsIgnoreCase("Dont crouch");
         }
--- src/main/java/com/sucy/skill/dynamic/trigger/ClickRightTrigger.java
+++ src/main/java/com/sucy/skill/dynamic/trigger/ClickRightTrigger.java
@@ -3,6 +3,7 @@ package com.sucy.skill.dynamic.trigger;
 import com.sucy.skill.api.Settings;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 public class ClickRightTrigger extends ClickTrigger {
 
@@ -19,8 +20,8 @@ public class ClickRightTrigger extends ClickTrigger {
      */
     @Override
     public boolean shouldTrigger(PlayerInteractEvent event, int level, Settings settings) {
-
-        if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+        if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)
+                && event.getHand() == EquipmentSlot.HAND) {
             return settings.getString("crouch").equalsIgnoreCase("both") ||
                     event.getPlayer().isSneaking() != settings.getString("crouch").equalsIgnoreCase("Dont crouch");
         }
