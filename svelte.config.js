64680951a8878f1fd2669dbede58adddc315a979
Travja <the.only.t.craft@gmail.com>
2023-01-11 02:34:33 -0700

    Use 404.html instead for SPA mode

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'gh-pages',
 			assets: 'gh-pages',
-			fallback: "index.html",
+			fallback: "404.html",
 			precompress: false,
 			strict: true
 		})
