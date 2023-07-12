284c90265ef209e2390c14a11bc07e4513a06315
Artem <nersesian.artem@gmail.com>
2023-07-12 09:10:02 +0200

    Docs inherited

--- src/main/java/com/sucy/skill/dynamic/trigger/ProjectileLandTrigger.java
+++ src/main/java/com/sucy/skill/dynamic/trigger/ProjectileLandTrigger.java
@@ -10,17 +10,30 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * ProSkillAPI © 2023
+ * com.sucy.skill.dynamic.trigger.ProjectileLandTrigger
+ */
 public class ProjectileLandTrigger implements Trigger<ProjectileHitEvent> {
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getKey() {
         return "PROJECTILE_LAND";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Class<ProjectileHitEvent> getEvent() {
         return ProjectileHitEvent.class;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean shouldTrigger(ProjectileHitEvent event, int level, Settings settings) {
         if(!Objects.isNull(event.getHitEntity())) return false;
@@ -30,15 +43,22 @@ public class ProjectileLandTrigger implements Trigger<ProjectileHitEvent> {
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setValues(ProjectileHitEvent event, Map<String, Object> data) {}
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public LivingEntity getCaster(ProjectileHitEvent event) {
         var shooter = event.getEntity().getShooter();
         return shooter instanceof LivingEntity? (LivingEntity) shooter : null;
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public LivingEntity getTarget(ProjectileHitEvent event, Settings settings) {
         Location loc = event.getEntity().getLocation();
