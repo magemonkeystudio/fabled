f26d5aaa74d7d62e21c80474f1a28c4fc5884cbd
Funayd <105151651+fructozer@users.noreply.github.com>
2023-08-01 23:45:12 +0700

    add check for potion type
    as the title says

--- src/main/java/com/sucy/skill/dynamic/trigger/ConsumeTrigger.java
+++ src/main/java/com/sucy/skill/dynamic/trigger/ConsumeTrigger.java
@@ -3,8 +3,8 @@ package com.sucy.skill.dynamic.trigger;
 import com.sucy.skill.api.Settings;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.meta.PotionMeta;
 
-import java.util.List;
 import java.util.Map;
 
 public class ConsumeTrigger implements Trigger<PlayerItemConsumeEvent>{
@@ -30,6 +30,11 @@ public class ConsumeTrigger implements Trigger<PlayerItemConsumeEvent>{
     public boolean shouldTrigger(PlayerItemConsumeEvent event, int level, Settings settings) {
         final String item_name = event.getItem().getType().name();
         final String req = settings.getString("material", "Any").replace(" ","_");
+        if (item_name.equalsIgnoreCase("potion") && req.equalsIgnoreCase(item_name)){
+            PotionMeta potion = (PotionMeta) event.getItem().getItemMeta();
+            String type = potion.getBasePotionData().getType().getEffectType().getName();
+            if (!settings.getString("potion", "Any").replace(" ","_").equalsIgnoreCase(type)) return false;
+        }
         return req.equalsIgnoreCase(item_name) || req.equalsIgnoreCase("Any");
     }
 
