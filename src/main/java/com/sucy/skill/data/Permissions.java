e4604ec7dc7e89f2a81f66a05fffa8b1459408da
Funayd <105151651+fructozer@users.noreply.github.com>
2023-08-02 13:53:25 +0700

    Revert "Merge branch 'dev' into local-dev"
    This reverts commit f41bbb4f49a38bbd9ed7bc20d7b7e6d4b5f4ea7d, reversing
    changes made to 2ec63774f072f6e5aa1e7f2e56e5b0d25fa02203.

--- src/main/java/com/sucy/skill/data/Permissions.java
+++ src/main/java/com/sucy/skill/data/Permissions.java
@@ -31,7 +31,6 @@ package com.sucy.skill.data;
  */
 public class Permissions {
     private static final String ROOT = "skillapi.";
-
     public static final String BASIC        = ROOT + "basic";
     public static final String BACKUP       = ROOT + "backup";
     public static final String EXP          = ROOT + "exp";
@@ -49,4 +48,5 @@ public class Permissions {
     public static final String RESET        = ROOT + "reset";
     public static final String REFUND       = ROOT + "refund";
     public static final String MAX_ACCOUNTS = ROOT + "max_accounts";
+    public static final String LOADED       = ROOT + "loaded";
 }
