dbdf79c681df6dc76695af02612e5ff680f29388
Funayd <105151651+fructozer@users.noreply.github.com>
2023-08-02 12:36:38 +0700

    some small change
    counting loaded amount is a stupid idea

--- src/main/java/com/sucy/skill/cmd/CmdLoaded.java
+++ src/main/java/com/sucy/skill/cmd/CmdLoaded.java
@@ -33,25 +33,23 @@ public class CmdLoaded implements IFunction {
     @Override
     public void execute(ConfigurableCommand cmd, Plugin plugin, CommandSender sender, String[] args) {
         if (args.length<1) {
-            cmd.sendMessage(sender, NO_ARGUMENT, "&4Missing argument! <class|skill>");
+            cmd.sendMessage(sender, NO_ARGUMENT, "&4Missing argument! <group|class|skill|attr>");
             return;
         }
-        AtomicInteger count = new AtomicInteger(1);
         switch (args[0]) {
 
-
-            case "group": sendList(cmd, sender, GROUP_HEAD,"-------Loaded %d group(s)-------", SkillAPI.getGroups()); break;
-            case "class": sendList(cmd, sender, CLASS_HEAD,"-------Loaded %d class(es)-------", SkillAPI.getClasses().keySet()); break;
-            case "skill": sendList(cmd, sender, SKILL_HEAD,"-------Loaded %d skill(s)-------", SkillAPI.getSkills().keySet()); break;
+            case "group": sendList(cmd, sender, GROUP_HEAD,"-------Loaded group(s)-------", SkillAPI.getGroups()); break;
+            case "class": sendList(cmd, sender, CLASS_HEAD,"-------Loaded class(es)-------", SkillAPI.getClasses().keySet()); break;
+            case "skill": sendList(cmd, sender, SKILL_HEAD,"-------Loaded skill(s)-------", SkillAPI.getSkills().keySet()); break;
             case "attr":
-            case "attribute": sendList(cmd, sender, ATTR_HEAD,"-------Loaded %d attribute(s)-------", SkillAPI.getAttributeManager().getKeys()); break;
+            case "attribute": sendList(cmd, sender, ATTR_HEAD,"-------Loaded attribute(s)-------", SkillAPI.getAttributeManager().getKeys()); break;
             default:
                 cmd.sendMessage(sender, INVALID_ARGUMENT, "&4Invalid argument! <group|class|skill|attr>");
         }
     }
     private static void sendList(ConfigurableCommand cmd, CommandSender sender,String key, String head, Collection<?> list){
         AtomicInteger count = new AtomicInteger(1);
-        cmd.sendMessage(sender, key, String.format(head, list.size()));
+        cmd.sendMessage(sender, key, head);
         list.forEach(entry -> sender.sendMessage(String.format("%d: %s", count.getAndIncrement(), entry)));
         cmd.sendMessage(sender, END, "-------------------------------");
     }
