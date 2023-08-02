be59dace5e32742bc0c02bf889a4b3eeec6d213b
Sentropic <sentropic.sv@gmail.com>
2023-08-02 17:05:33 -0600

    Add arg support in batch file

--- execute.bat
+++ execute.bat
@@ -1,3 +1,5 @@
+@ECHO off
 set /p Input=Enter the Spigot Version (Example: 1.19): 
-call mvn clean install exec:java -Dspigot-version=%Input% -Dexec.args=%Input%
+set /p Args=Enter any additional arguments: 
+call mvn clean install exec:java -Dspigot-version=%Input% -Dexec.args="%Input% %Args%"
 pause
