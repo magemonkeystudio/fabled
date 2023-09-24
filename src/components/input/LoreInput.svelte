a15b028afda10716b3f8a93c68cad58947f2e033
Travja <the.only.t.craft@gmail.com>
2023-09-24 07:53:49 -0600

    Fix multiline input in Firefox

--- src/components/input/LoreInput.svelte
+++ src/components/input/LoreInput.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
   export let value: string[] = [];
-  export let wrap = false;
-
   $: if (!value) value = [];
 
-  const press = (e: KeyboardEvent) => value = (<HTMLTextAreaElement>e.target).value.split(/\r?\n/g);
+  const press = (e: KeyboardEvent) => value = (<HTMLTextAreaElement>e.target).value.split(/\r?\n/g)
 </script>
 
 <textarea rows="5" value={value.join("\r\n")}
-          style:white-space={wrap ? "wrap" : "nowrap"}
           on:keyup={press} />
 
 <style>
