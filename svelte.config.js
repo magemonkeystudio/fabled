17698e130b1bd825ddc6e3c3fd1ae8b4ae1a0b59
Travja <the.only.t.craft@gmail.com>
2023-09-24 14:53:34 -0600

    Make this work on gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,34 @@
-import adapter from "@sveltejs/adapter-static";
-import preprocess from "svelte-preprocess";
-import { resolve } from "path";
+import adapter     from '@sveltejs/adapter-static';
+import preprocess  from 'svelte-preprocess';
+import { resolve } from 'path';
 
-/** @type {import("@sveltejs/kit").Config} */
+const dev = process.argv.includes('dev');
+
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
 
-  kit: {
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: "gh-pages",
-      assets: "gh-pages",
-      fallback: "404.html",
-      precompress: false,
-      strict: true
-    }),
-    alias: {
-      $api: resolve("./src/api"),
-      $input: resolve("./src/components/input"),
-      $components: resolve("./src/components")
-    }
-  }
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages:       'gh-pages',
+			assets:      'gh-pages',
+			fallback:    '404.html',
+			precompress: false,
+			strict:      true
+		}),
+		paths:   {
+			base: dev ? '' : '/proskillapi'
+		},
+		alias:   {
+			$api:        resolve('./src/api'),
+			$input:      resolve('./src/components/input'),
+			$components: resolve('./src/components')
+		}
+	}
 };
 
 export default config;
