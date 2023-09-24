d71a11d659bea570fe6eb91b06c9ed392deae3ca
Travja <the.only.t.craft@gmail.com>
2023-09-24 15:58:04 -0600

    Load old version data

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,41 +1,50 @@
-import type ProFolder from "./profolder";
-import type ProClass from "./proclass";
-import type ProSkill from "./proskill";
-import { browser } from "$app/environment";
-import { writable } from "svelte/store";
+import type ProFolder from './profolder';
+import type ProClass  from './proclass';
+import type ProSkill  from './proskill';
+import { browser }    from '$app/environment';
+import { writable }   from 'svelte/store';
 
 export const toProperCase = (s: string) => {
-  return s.replace("_", " ").toLowerCase()
-    .replace(/^(.)|\s(.)/g, $1 => $1.toUpperCase());
+	return s.replace('_', ' ').toLowerCase()
+		.replace(/^(.)|\s(.)/g, $1 => $1.toUpperCase());
 };
 
 export const toEditorCase = (s: string) => {
-  s = s.replace("_", " ");
-  return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
+	s = s.replace('_', ' ');
+	return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
 };
 
 export const sort = <T extends ProFolder | ProClass | ProSkill>(data: T[]): T[] => {
-  return data.sort((a, b) => a.name.localeCompare(b.name));
+	return data.sort((a, b) => a.name.localeCompare(b.name));
 };
 
 export const localStore = <T>(key: string, def: T) => {
-  let saved: T = def;
-  if (browser) {
-    if (localStorage.getItem(key) === null) {
-      localStorage.setItem(key, JSON.stringify(def));
-    }
+	let saved: T = def;
+	if (browser) {
+		if (localStorage.getItem(key) === null) {
+			if (typeof def === 'string' || typeof def === 'number' || typeof def === 'boolean') {
+				localStorage.setItem(key, def.toString());
+			} else {
+				localStorage.setItem(key, JSON.stringify(def));
+			}
+		}
 
-    saved = JSON.parse(<string>localStorage.getItem(key));
-  }
+		saved = JSON.parse(<string>localStorage.getItem(key));
+	}
 
-  const { subscribe, set, update } = writable(saved);
+	const { subscribe, set, update } = writable(saved);
 
-  return {
-    subscribe,
-    set: (value: T) => {
-      if (browser) localStorage.setItem(key, JSON.stringify(value));
-      return set(value);
-    },
-    update
-  };
+	return {
+		subscribe,
+		set: (value: T) => {		if (browser) {
+				if (typeof value === 'string' || typeof value === 'number' || typeof value === 'boolean') {
+					localStorage.setItem(key, value.toString());
+				} else {
+					localStorage.setItem(key, JSON.stringify(value));
+				}
+			}
+			return set(value);
+		},
+		update
+	};
 };
--- src/version/data.ts
+++ src/version/data.ts
@@ -1,164 +1,172 @@
-import type { VersionData } from "$api/types";
-import type { Writable } from "svelte/store";
-import { get, writable } from "svelte/store";
-import { DATA_1_20 } from "./1.20";
-import { DATA_1_19 } from "./1.19";
-import { DATA_1_18 } from "./1.18";
-import { DATA_1_17 } from "./1.17";
-import { DATA_1_16 } from "./1.16";
-import { localStore } from "$api/api";
+import type { VersionData } from '$api/types';
+import type { Writable }    from 'svelte/store';
+import { get, writable }    from 'svelte/store';
+import { DATA_1_20 }        from './1.20';
+import { DATA_1_19 }        from './1.19';
+import { DATA_1_18 }        from './1.18';
+import { DATA_1_17 }        from './1.17';
+import { DATA_1_16 }        from './1.16';
+import { localStore }       from '$api/api';
 
-export const expSources = ["Mob", "Block Break", "Block Place", "Craft", "Command", "Special", "Exp Bottle", "Smelt", "Quest"];
+export const expSources = ['Mob', 'Block Break', 'Block Place', 'Craft', 'Command', 'Special', 'Exp Bottle', 'Smelt', 'Quest'];
 
-type Versions = "20" | "19" | "18" | "17" | "16";
+type Versions = 20 | 19 | 18 | 17 | 16;
 
-const VERSIONS = {
-  "20": DATA_1_20,
-  "19": DATA_1_19,
-  "18": DATA_1_18,
-  "17": DATA_1_17,
-  "16": DATA_1_16
+const VERSIONS              = {
+	20: DATA_1_20,
+	19: DATA_1_19,
+	18: DATA_1_18,
+	17: DATA_1_17,
+	16: DATA_1_16
 };
 const versionKeys: string[] = Object.keys(VERSIONS).reverse();
 
-export const versionData: Writable<VersionData> = writable(VERSIONS[<Versions>versionKeys[0]]);
+export const versionData: Writable<VersionData> = writable(VERSIONS[<Versions>parseInt(versionKeys[0])]);
 
 export const serverOptions: string[] = [];
-versionKeys.forEach((v: string) => serverOptions.push("1." + v));
-export const version: Writable<Versions> = localStore<Versions>("server-version", <Versions>versionKeys[0]);
-version.subscribe((ver: Versions) => versionData.set(VERSIONS[ver]));
+versionKeys.forEach((v: string) => serverOptions.push('1.' + v));
+export const version: Writable<Versions> = localStore<Versions>('server-version', <Versions>parseInt(versionKeys[0]));
+version.subscribe((ver: Versions) => {
+	if (!(ver in VERSIONS)) {
+		ver = <Versions>parseInt(versionKeys[0]);
+		version.set(ver);
+		return;
+	}
+
+	versionData.set(VERSIONS[ver]);
+});
 
 export const getMaterials = () => {
-  return get(versionData).MATERIALS;
+	return get(versionData).MATERIALS;
 };
 
 export const getBlocks = () => {
-  return get(versionData).BLOCKS;
+	return get(versionData).BLOCKS;
 };
 
 export const getDamageableMaterials = () => {
-  return get(versionData).DAMAGEABLE_MATERIALS;
+	return get(versionData).DAMAGEABLE_MATERIALS;
 };
 
 export const getAnyMaterials = () => {
-  return ["Any", ...get(versionData).MATERIALS];
+	return ['Any', ...get(versionData).MATERIALS];
 };
 
 export const getSounds = () => {
-  return get(versionData).SOUNDS;
+	return get(versionData).SOUNDS;
 };
 
 export const getEntities = () => {
-  return get(versionData).ENTITIES;
+	return get(versionData).ENTITIES;
 };
 
 export const getAnyEntities = () => {
-  return ["Any", ...get(versionData).ENTITIES];
+	return ['Any', ...get(versionData).ENTITIES];
 };
 
 export const getParticles = () => {
-  return get(versionData).PARTICLES || [];
+	return get(versionData).PARTICLES || [];
 };
 
 export const getBiomes = () => {
-  return get(versionData).BIOMES;
+	return get(versionData).BIOMES;
 };
 
 export const getDamageTypes = () => {
-  return get(versionData).DAMAGE_TYPES;
+	return get(versionData).DAMAGE_TYPES;
 };
 
 export const getPotionTypes = () => {
-  return get(versionData).POTIONS;
+	return get(versionData).POTIONS;
 };
 
 export const getAnyPotion = () => {
-  return ["Any", ...get(versionData).POTIONS];
+	return ['Any', ...get(versionData).POTIONS];
 };
 
 export const getAnyConsumable = () => {
-  return ["Any", ...get(versionData).CONSUMABLE];
+	return ['Any', ...get(versionData).CONSUMABLE];
 };
 
 export const getGoodPotions = () => {
-  const list = get(versionData).POTIONS.filter(type => GOOD_POTIONS.includes(type));
-  return ["All", ...list];
+	const list = get(versionData).POTIONS.filter(type => GOOD_POTIONS.includes(type));
+	return ['All', ...list];
 };
 
 export const getBadPotions = () => {
-  const list = get(versionData).POTIONS.filter(type => BAD_POTIONS.includes(type));
-  return ["All", ...list];
+	const list = get(versionData).POTIONS.filter(type => BAD_POTIONS.includes(type));
+	return ['All', ...list];
 };
 
 export const getDyes = () => {
-  return DYES;
+	return DYES;
 };
 
 export const getProjectiles = () => {
-  return get(versionData).PROJECTILES;
+	return get(versionData).PROJECTILES;
 };
 
 export const getAnyProjectiles = () => {
-  return ["Any", ...get(versionData).PROJECTILES];
+	return ['Any', ...get(versionData).PROJECTILES];
 };
 
 export const getMobDisguises = () => {
-  return get(versionData).MOB_DISGUISES;
+	return get(versionData).MOB_DISGUISES;
 };
 
 export const getMiscDisguises = () => {
-  return get(versionData).MISC_DISGUISES;
+	return get(versionData).MISC_DISGUISES;
 };
 
 const GOOD_POTIONS: string[] = [
-  "Speed",
-  "Fast Digging",
-  "Increase Damage",
-  "Jump",
-  "Regeneration",
-  "Damage Resistance",
-  "Fire Resistance",
-  "Water Breathing",
-  "Invisibility",
-  "Night Vision",
-  "Health Boost",
-  "Absorption",
-  "Saturation",
-  "Glowing",
-  "Luck",
-  "Slow Falling",
-  "Conduit Power",
-  "Dolphins Grace"
+	'Speed',
+	'Fast Digging',
+	'Increase Damage',
+	'Jump',
+	'Regeneration',
+	'Damage Resistance',
+	'Fire Resistance',
+	'Water Breathing',
+	'Invisibility',
+	'Night Vision',
+	'Health Boost',
+	'Absorption',
+	'Saturation',
+	'Glowing',
+	'Luck',
+	'Slow Falling',
+	'Conduit Power',
+	'Dolphins Grace'
 ];
 
 const BAD_POTIONS: string[] = [
-  "Slow",
-  "Slow Digging",
-  "Confusion",
-  "Blindness",
-  "Hunger",
-  "Weakness",
-  "Poison",
-  "Wither",
-  "Levitation",
-  "Unluck"
+	'Slow',
+	'Slow Digging',
+	'Confusion',
+	'Blindness',
+	'Hunger',
+	'Weakness',
+	'Poison',
+	'Wither',
+	'Levitation',
+	'Unluck'
 ];
 
 const DYES: string[] = [
-  "BLACK",
-  "BLUE",
-  "BROWN",
-  "CYAN",
-  "GRAY",
-  "GREEN",
-  "LIGHT_BLUE",
-  "LIGHT_GRAY",
-  "LIME",
-  "MAGENTA",
-  "ORANGE",
-  "PINK",
-  "PURPLE",
-  "RED",
-  "WHITE",
-  "YELLOW"
+	'BLACK',
+	'BLUE',
+	'BROWN',
+	'CYAN',
+	'GRAY',
+	'GREEN',
+	'LIGHT_BLUE',
+	'LIGHT_GRAY',
+	'LIME',
+	'MAGENTA',
+	'ORANGE',
+	'PINK',
+	'PURPLE',
+	'RED',
+	'WHITE',
+	'YELLOW'
 ];
