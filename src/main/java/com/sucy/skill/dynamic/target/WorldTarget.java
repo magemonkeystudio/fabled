8cb83aed23b9cb932b3bccaf56a98043c7ac9a1c
Travja <the.only.t.craft@gmail.com>
2023-09-25 00:07:30 -0600

    Add WorldTarget

---
+++ src/main/java/com/sucy/skill/dynamic/target/WorldTarget.java
@@ -0,0 +1,67 @@
+/*
+ * ProSkillAPI
+ * com.sucy.skill.dynamic.target.WorldTarget
+ * <p>
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2023 ProMCTeam
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software") to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.sucy.skill.dynamic.target;
+
+import org.bukkit.GameMode;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Applies child components to all entities in the world.
+ */
+public class WorldTarget extends TargetComponent {
+    @Override
+    public String getKey() {
+        return "world";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    List<LivingEntity> getTargets(
+            final LivingEntity caster, final int level, final List<LivingEntity> targets) {
+        final Set<LivingEntity> result = new HashSet<>();
+        for (LivingEntity target : targets) {
+            target.getWorld().getLivingEntities().stream()
+                    .filter(e -> {
+                        GameMode gm = e instanceof Player ? ((Player) e).getGameMode() : GameMode.SURVIVAL;
+                        if (gm == GameMode.SPECTATOR || gm == GameMode.CREATIVE) return false;
+
+                        return true;
+                    })
+                    .forEach(result::add);
+
+        }
+        return result.stream().collect(Collectors.toList());
+    }
+}
