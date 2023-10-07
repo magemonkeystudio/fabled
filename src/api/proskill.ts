92aebe3e117341920c6b3af59215d45b9e4fbe4a
Travja <the.only.t.craft@gmail.com>
2023-10-06 22:46:15 -0600

    Fix cooldown-message always exporting as true

--- src/api/proskill.ts
+++ src/api/proskill.ts
@@ -1,164 +1,164 @@
-import type {Icon, ProSkillData, Serializable} from "./types";
-import {YAMLObject} from "./yaml";
-import {ProAttribute} from "./proattribute";
-import {toEditorCase} from "./api";
-import {getSkill} from "../data/skill-store";
-import ProTrigger from "./components/triggers";
-import type ProComponent from "$api/components/procomponent";
-import Registry from "$api/components/registry";
-import type {Unsubscriber} from "svelte/types/runtime/store";
+import type { Icon, ProSkillData, Serializable } from './types';
+import { YAMLObject }                            from './yaml';
+import { ProAttribute }                          from './proattribute';
+import { toEditorCase }                          from './api';
+import { getSkill }                              from '../data/skill-store';
+import ProTrigger                                from './components/triggers';
+import type ProComponent                         from '$api/components/procomponent';
+import Registry                                  from '$api/components/registry';
+import type { Unsubscriber }                     from 'svelte/types/runtime/store';
 
 export default class ProSkill implements Serializable {
-    isSkill = true;
-    public key = {};
-    name: string;
-    type = "Dynamic";
-    maxLevel = 5;
-    skillReq?: ProSkill;
-    skillReqLevel = 0;
-    permission = false;
-    levelReq: ProAttribute = new ProAttribute("level", 1, 0);
-    cost: ProAttribute = new ProAttribute("cost", 1, 0);
-    cooldown: ProAttribute = new ProAttribute("cooldown", 0, 0);
-    cooldownMessage = true;
-    mana: ProAttribute = new ProAttribute("mana", 0, 0);
-    minSpent: ProAttribute = new ProAttribute("points-spent-req", 0, 0);
-    castMessage = "&6{player} &2has cast &6{skill}";
-    combo = "";
-    icon: Icon = {
-        material: "Pumpkin",
-        customModelData: 0,
-        lore: [
-            "&d{name} &7({level}/{max})",
-            "&2Type: &6{type}",
-            "",
-            "{req:level}Level: {attr:level}",
-            "{req:cost}Cost: {attr:cost}",
-            "",
-            "&2Mana: {attr:mana}",
-            "&2Cooldown: {attr:cooldown}"
-        ]
-    };
-    incompatible: ProSkill[] = [];
-    triggers: ProTrigger[] = [];
+	isSkill                  = true;
+	public key               = {};
+	name: string;
+	type                     = 'Dynamic';
+	maxLevel                 = 5;
+	skillReq?: ProSkill;
+	skillReqLevel            = 0;
+	permission: boolean      = false;
+	levelReq: ProAttribute   = new ProAttribute('level', 1, 0);
+	cost: ProAttribute       = new ProAttribute('cost', 1, 0);
+	cooldown: ProAttribute   = new ProAttribute('cooldown', 0, 0);
+	cooldownMessage: boolean = true;
+	mana: ProAttribute       = new ProAttribute('mana', 0, 0);
+	minSpent: ProAttribute   = new ProAttribute('points-spent-req', 0, 0);
+	castMessage              = '&6{player} &2has cast &6{skill}';
+	combo                    = '';
+	icon: Icon               = {
+		material:        'Pumpkin',
+		customModelData: 0,
+		lore:            [
+			'&d{name} &7({level}/{max})',
+			'&2Type: &6{type}',
+			'',
+			'{req:level}Level: {attr:level}',
+			'{req:cost}Cost: {attr:cost}',
+			'',
+			'&2Mana: {attr:mana}',
+			'&2Cooldown: {attr:cooldown}'
+		]
+	};
+	incompatible: ProSkill[] = [];
+	triggers: ProTrigger[]   = [];
 
-    private skillReqStr = "";
-    private incompStr: string[] = [];
+	private skillReqStr         = '';
+	private incompStr: string[] = [];
 
-    constructor(data?: ProSkillData) {
-        this.name = data ? data.name : "Skill";
-        if (data?.type) this.type = data.type;
-        if (data?.maxLevel) this.maxLevel = data.maxLevel;
-        if (data?.skillReq) this.skillReq = data.skillReq;
-        if (data?.skillReqLevel) this.skillReqLevel = data.skillReqLevel;
-        if (data?.permission) this.permission = data.permission;
-        if (data?.levelReq) this.levelReq = data.levelReq;
-        if (data?.cost) this.cost = data.cost;
-        if (data?.cooldown) this.cooldown = data.cooldown;
-        if (data?.cooldownMessage) this.cooldownMessage = data.cooldownMessage;
-        if (data?.mana) this.mana = data.mana;
-        if (data?.minSpent) this.minSpent = data.minSpent;
-        if (data?.castMessage) this.castMessage = data.castMessage;
-        if (data?.combo) this.combo = data.combo;
-        if (data?.icon) this.icon = data.icon;
-        if (data?.incompatible) this.incompatible = data.incompatible;
-        if (data?.triggers) this.triggers = data.triggers;
-    }
+	constructor(data?: ProSkillData) {
+		this.name = data ? data.name : 'Skill';
+		if (data?.type) this.type = data.type;
+		if (data?.maxLevel) this.maxLevel = data.maxLevel;
+		if (data?.skillReq) this.skillReq = data.skillReq;
+		if (data?.skillReqLevel) this.skillReqLevel = data.skillReqLevel;
+		if (data?.permission) this.permission = data.permission;
+		if (data?.levelReq) this.levelReq = data.levelReq;
+		if (data?.cost) this.cost = data.cost;
+		if (data?.cooldown) this.cooldown = data.cooldown;
+		if (data?.cooldownMessage) this.cooldownMessage = data.cooldownMessage;
+		if (data?.mana) this.mana = data.mana;
+		if (data?.minSpent) this.minSpent = data.minSpent;
+		if (data?.castMessage) this.castMessage = data.castMessage;
+		if (data?.combo) this.combo = data.combo;
+		if (data?.icon) this.icon = data.icon;
+		if (data?.incompatible) this.incompatible = data.incompatible;
+		if (data?.triggers) this.triggers = data.triggers;
+	}
 
-    public addComponent = (comp: ProComponent) => {
-        if (comp instanceof ProTrigger) {
-            this.triggers = [...this.triggers, comp];
-            return;
-        }
+	public addComponent = (comp: ProComponent) => {
+		if (comp instanceof ProTrigger) {
+			this.triggers = [...this.triggers, comp];
+			return;
+		}
 
-        if (this.triggers.length === 0) {
-            this.triggers.push(<ProTrigger>Registry.getTriggerByName("cast")?.new());
-        }
+		if (this.triggers.length === 0) {
+			this.triggers.push(<ProTrigger>Registry.getTriggerByName('cast')?.new());
+		}
 
-        this.triggers[0].addComponent(comp);
-        this.triggers = [...this.triggers];
-    };
+		this.triggers[0].addComponent(comp);
+		this.triggers = [...this.triggers];
+	};
 
-    public removeComponent = (comp: ProComponent) => {
-        if (comp instanceof ProTrigger && this.triggers.includes(comp)) {
-            this.triggers.splice(this.triggers.indexOf(comp), 1);
-            return;
-        }
+	public removeComponent = (comp: ProComponent) => {
+		if (comp instanceof ProTrigger && this.triggers.includes(comp)) {
+			this.triggers.splice(this.triggers.indexOf(comp), 1);
+			return;
+		}
 
-        for (const trigger of this.triggers) {
-            if (trigger.contains(comp))
-                trigger.removeComponent(comp);
-        }
+		for (const trigger of this.triggers) {
+			if (trigger.contains(comp))
+				trigger.removeComponent(comp);
+		}
 
-        this.triggers = [...this.triggers];
-    };
+		this.triggers = [...this.triggers];
+	};
 
-    public serializeYaml = (): YAMLObject => {
-        const yaml = new YAMLObject(this.name);
-        const data = new YAMLObject();
-        data.put("name", this.name);
-        data.put("type", this.type);
-        data.put("max-level", this.maxLevel);
-        data.put("skill-req", this.skillReq?.name);
-        data.put("skill-req-lvl", this.skillReqLevel);
-        data.put("needs-permission", this.permission);
-        data.put("cooldown-message", this.cooldownMessage);
-        data.put("msg", this.castMessage);
-        data.put("combo", this.combo);
-        data.put("icon", this.icon.material);
-        data.put("icon-data", this.icon.customModelData);
-        data.put("icon-lore", this.icon.lore);
-        const attributes = new YAMLObject("attributes");
-        attributes.put("level", this.levelReq);
-        attributes.put("cost", this.cost);
-        attributes.put("cooldown", this.cooldown);
-        attributes.put("mana", this.mana);
-        attributes.put("points-spent-req", this.minSpent);
-        data.put("attributes", attributes);
-        data.put("incompatible", this.incompatible.map(s => s.name));
-        data.put("components", this.triggers);
+	public serializeYaml = (): YAMLObject => {
+		const yaml = new YAMLObject(this.name);
+		const data = new YAMLObject();
+		data.put('name', this.name);
+		data.put('type', this.type);
+		data.put('max-level', this.maxLevel);
+		data.put('skill-req', this.skillReq?.name);
+		data.put('skill-req-lvl', this.skillReqLevel);
+		data.put('needs-permission', this.permission);
+		data.put('cooldown-message', this.cooldownMessage);
+		data.put('msg', this.castMessage);
+		data.put('combo', this.combo);
+		data.put('icon', this.icon.material);
+		data.put('icon-data', this.icon.customModelData);
+		data.put('icon-lore', this.icon.lore);
+		const attributes = new YAMLObject('attributes');
+		attributes.put('level', this.levelReq);
+		attributes.put('cost', this.cost);
+		attributes.put('cooldown', this.cooldown);
+		attributes.put('mana', this.mana);
+		attributes.put('points-spent-req', this.minSpent);
+		data.put('attributes', attributes);
+		data.put('incompatible', this.incompatible.map(s => s.name));
+		data.put('components', this.triggers);
 
-        yaml.data = data.data;
-        return yaml;
-    };
+		yaml.data = data.data;
+		return yaml;
+	};
 
-    public load = (yaml: YAMLObject) => {
-        this.name = yaml.get("name", this.name);
-        this.type = yaml.get("type", this.type);
-        this.maxLevel = yaml.get("max-level", this.maxLevel);
-        this.skillReqStr = yaml.get("skill-req", this.skillReqStr);
-        this.skillReqLevel = yaml.get("skill-req-lvl", this.skillReqLevel);
-        this.permission = yaml.get("needs-permission", this.permission);
-        this.cooldownMessage = yaml.get("cooldown-message", this.cooldownMessage);
-        this.castMessage = yaml.get("msg", this.castMessage);
-        this.combo = yaml.get("combo", this.combo);
+	public load = (yaml: YAMLObject) => {
+		this.name            = yaml.get('name', this.name);
+		this.type            = yaml.get('type', this.type);
+		this.maxLevel        = yaml.get('max-level', this.maxLevel);
+		this.skillReqStr     = yaml.get('skill-req', this.skillReqStr);
+		this.skillReqLevel   = yaml.get('skill-req-lvl', this.skillReqLevel);
+		this.permission      = yaml.get('needs-permission', this.permission);
+		this.cooldownMessage = yaml.get('cooldown-message', this.cooldownMessage);
+		this.castMessage     = yaml.get('msg', this.castMessage);
+		this.combo           = yaml.get('combo', this.combo);
 
-        const attributes: YAMLObject = yaml.get("attributes");
-        this.levelReq = new ProAttribute("level", attributes.get("level-base"), attributes.get("level-scale"));
-        this.cost = new ProAttribute("cost", attributes.get("cost-base"), attributes.get("cost-scale"));
-        this.cooldown = new ProAttribute("cooldown", attributes.get("cooldown-base"), attributes.get("cooldown-scale"));
-        this.mana = new ProAttribute("mana", attributes.get("mana-base"), attributes.get("mana-scale"));
-        this.minSpent = new ProAttribute("points-spent-req", attributes.get("points-spent-req-base"), attributes.get("points-spent-req-scale"));
+		const attributes: YAMLObject = yaml.get('attributes');
+		this.levelReq                = new ProAttribute('level', attributes.get('level-base'), attributes.get('level-scale'));
+		this.cost                    = new ProAttribute('cost', attributes.get('cost-base'), attributes.get('cost-scale'));
+		this.cooldown                = new ProAttribute('cooldown', attributes.get('cooldown-base'), attributes.get('cooldown-scale'));
+		this.mana                    = new ProAttribute('mana', attributes.get('mana-base'), attributes.get('mana-scale'));
+		this.minSpent                = new ProAttribute('points-spent-req', attributes.get('points-spent-req-base'), attributes.get('points-spent-req-scale'));
 
-        this.icon.material = yaml.get<string, string>("icon", this.icon.material, toEditorCase);
-        this.icon.customModelData = yaml.get("icon-data", this.icon.customModelData);
-        this.icon.lore = yaml.get("icon-lore", this.icon.lore);
-        this.incompStr = yaml.get("incompatible", this.incompStr);
+		this.icon.material        = yaml.get<string, string>('icon', this.icon.material, toEditorCase);
+		this.icon.customModelData = yaml.get('icon-data', this.icon.customModelData);
+		this.icon.lore            = yaml.get('icon-lore', this.icon.lore);
+		this.incompStr            = yaml.get('incompatible', this.incompStr);
 
-        let unsub: Unsubscriber | undefined = undefined;
+		let unsub: Unsubscriber | undefined = undefined;
 
-        unsub = Registry.initialized.subscribe(init => {
-            if (!init) return;
-            this.triggers = yaml.get<YAMLObject, ProTrigger[]>("components", this.triggers, (list: YAMLObject) => Registry.deserializeComponents(list));
+		unsub = Registry.initialized.subscribe(init => {
+			if (!init) return;
+			this.triggers = yaml.get<YAMLObject, ProTrigger[]>('components', this.triggers, (list: YAMLObject) => Registry.deserializeComponents(list));
 
-            if (unsub) {
-                unsub();
-            }
-        });
-    };
+			if (unsub) {
+				unsub();
+			}
+		});
+	};
 
-    public postLoad = () => {
-        this.skillReq = getSkill(this.skillReqStr);
-        this.incompatible = <ProSkill[]>this.incompStr.map(s => getSkill(s)).filter(s => !!s);
-    };
+	public postLoad = () => {
+		this.skillReq     = getSkill(this.skillReqStr);
+		this.incompatible = <ProSkill[]>this.incompStr.map(s => getSkill(s)).filter(s => !!s);
+	};
 }
--- src/components/SkillDetails.svelte
+++ src/components/SkillDetails.svelte
@@ -1,80 +1,81 @@
-<script lang="ts">
-  import { saveDataInternal, updateSidebar } from "../data/store";
-  import ProInput from "./input/ProInput.svelte";
-  import ProSkill from "$api/proskill";
-  import SearchableSelect from "./input/SearchableSelect.svelte";
-  import Toggle from "./input/Toggle.svelte";
-  import AttributeInput from "./input/AttributeInput.svelte";
-  import IconInput from "./input/IconInput.svelte";
-  import { skills } from "../data/skill-store";
+<script lang='ts'>
+	import { saveDataInternal, updateSidebar } from '../data/store';
+	import ProInput                            from './input/ProInput.svelte';
+	import type ProSkill                       from '$api/proskill';
+	import SearchableSelect                    from './input/SearchableSelect.svelte';
+	import Toggle                              from './input/Toggle.svelte';
+	import AttributeInput                      from './input/AttributeInput.svelte';
+	import IconInput                           from './input/IconInput.svelte';
+	import { skills }                          from '../data/skill-store';
 
-  export let data: ProSkill;
+	export let data: ProSkill;
 
-  $: {
-    if (data?.name) updateSidebar();
-    saveDataInternal();
-  }
+	$: {
+		if (data?.name) updateSidebar();
+		saveDataInternal();
+	}
 </script>
 
 {#if data}
-  <ProInput label="Name"
-            tooltip="The name of the skill. This should not contain color codes"
-            bind:value={data.name} />
-  <ProInput label="Type"
-            tooltip='The flavor text describing the skill such as "AOE utility" or whatever you want it to be'
-            bind:value={data.type} />
-  <ProInput label="Max Level" type="number"
-            tooltip="The maximum level the skill can reach"
-            bind:value={data.maxLevel} />
-  <ProInput label="Skill Requirement"
-            tooltip="The skill that needs to be upgraded before this one can be unlocked">
-    <SearchableSelect bind:selected={data.skillReq}
-                      data={$skills.filter(s => s !== data)}
-                      display={(s) => s.name} />
-  </ProInput>
-  <ProInput label="Skill Req Level" type="number" intMode
-            tooltip="The level that the required skill needs to reach before this one can be unlocked"
-            bind:value={data.skillReqLevel} />
-  <ProInput label="Permission"
-            tooltip='Whether this skill requires a permission to unlock. The permission would be "skillapi.skill.{data.name}"'>
-    <Toggle bind:data={data.permission} />
-  </ProInput>
-  <ProInput label="Level Req"
-            tooltip="The class level the player needs to be before unlocking or upgrading this skill">
-    <AttributeInput bind:value={data.levelReq} />
-  </ProInput>
-  <ProInput label="Cost"
-            tooltip="The amount of skill points needed to unlock and upgrade this skill">
-    <AttributeInput bind:value={data.cost} />
-  </ProInput>
-  <ProInput label="Cooldown"
-            tooltip="The time in seconds before the skill can be cast again (only works with the Cast trigger)">
-    <AttributeInput bind:value={data.cooldown} />
-  </ProInput>
-  <ProInput label="Cooldown Message"
-            tooltip="Whether to send a message when attempting to run the skill while in cooldown">
-    <Toggle bind:value={data.cooldownMessage} />
-  </ProInput>
-  <ProInput label="Mana"
-            tooltip="The amount of mana it takes to cast the skill (only works with the Cast trigger)">
-    <AttributeInput bind:value={data.mana} />
-  </ProInput>
-  <ProInput label="Min Spent"
-            tooltip="The amount of skill points that need to be spent before upgrading this skill">
-    <AttributeInput bind:value={data.minSpent} />
-  </ProInput>
-  <ProInput label="Cast Message"
-            tooltip="The message to display to players around the caster when the skill is cast. The radius of the area is in the config.yml options"
-            bind:value={data.castMessage} />
-  <ProInput label="Combo"
-            tooltip='The click combo to assign the skill (if enabled). Use L, R, S, LS, RS, P, Q and F for the types of clicks separated by spaces. For example, "L L R R" would work for 4 click combos.'
-            bind:value={data.combo} />
-  <IconInput bind:icon={data.icon} />
-  <ProInput label="Incompatible"
-            tooltip="List of skills that must not be upgraded in order to upgrade this skill">
-    <SearchableSelect multiple={true}
-                      data={$skills.filter(s => s!== data)}
-                      display={(s) => s.name}
-                      bind:selected={data.incompatible} />
-  </ProInput>
+	<ProInput label='Name'
+						tooltip='The name of the skill. This should not contain color codes'
+						bind:value={data.name} />
+	<ProInput label='Type'
+						tooltip='The flavor text describing the skill such as "AOE utility" or whatever you want it to be'
+						bind:value={data.type} />
+	<ProInput label='Max Level' type='number'
+						tooltip='The maximum level the skill can reach'
+						bind:value={data.maxLevel} />
+	<ProInput label='Skill Requirement'
+						tooltip='The skill that needs to be upgraded before this one can be unlocked'>
+		<SearchableSelect id='skillReq'
+											bind:selected={data.skillReq}
+											data={$skills.filter(s => s !== data)}
+											display={(s) => s.name} />
+	</ProInput>
+	<ProInput label='Skill Req Level' type='number' intMode
+						tooltip='The level that the required skill needs to reach before this one can be unlocked'
+						bind:value={data.skillReqLevel} />
+	<ProInput label='Permission'
+						tooltip='Whether this skill requires a permission to unlock. The permission would be "skillapi.skill.{data.name}"'>
+		<Toggle bind:data={data.permission} />
+	</ProInput>
+	<ProInput label='Level Req'
+						tooltip='The class level the player needs to be before unlocking or upgrading this skill'>
+		<AttributeInput bind:value={data.levelReq} />
+	</ProInput>
+	<ProInput label='Cost'
+						tooltip='The amount of skill points needed to unlock and upgrade this skill'>
+		<AttributeInput bind:value={data.cost} />
+	</ProInput>
+	<ProInput label='Cooldown'
+						tooltip='The time in seconds before the skill can be cast again (only works with the Cast trigger)'>
+		<AttributeInput bind:value={data.cooldown} />
+	</ProInput>
+	<ProInput label='Cooldown Message'
+						tooltip='Whether to send a message when attempting to run the skill while in cooldown'>
+		<Toggle bind:data={data.cooldownMessage} />
+	</ProInput>
+	<ProInput label='Mana'
+						tooltip='The amount of mana it takes to cast the skill (only works with the Cast trigger)'>
+		<AttributeInput bind:value={data.mana} />
+	</ProInput>
+	<ProInput label='Min Spent'
+						tooltip='The amount of skill points that need to be spent before upgrading this skill'>
+		<AttributeInput bind:value={data.minSpent} />
+	</ProInput>
+	<ProInput label='Cast Message'
+						tooltip='The message to display to players around the caster when the skill is cast. The radius of the area is in the config.yml options'
+						bind:value={data.castMessage} />
+	<ProInput label='Combo'
+						tooltip='The click combo to assign the skill (if enabled). Use L, R, S, LS, RS, P, Q and F for the types of clicks separated by spaces. For example, "L L R R" would work for 4 click combos.'
+						bind:value={data.combo} />
+	<IconInput bind:icon={data.icon} />
+	<ProInput label='Incompatible'
+						tooltip='List of skills that must not be upgraded in order to upgrade this skill'>
+		<SearchableSelect multiple={true}
+											data={$skills.filter(s => s!== data)}
+											display={(s) => s.name}
+											bind:selected={data.incompatible} />
+	</ProInput>
 {/if}
