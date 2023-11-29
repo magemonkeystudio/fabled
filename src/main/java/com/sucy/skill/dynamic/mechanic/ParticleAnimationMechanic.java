50cb10905aaf285f4a1b7967732b5d9015f0b7b7
Travja <the.only.t.craft@gmail.com>
2023-11-28 20:17:10 -0700

    Actually properly fix particle animation offset

--- src/main/java/com/sucy/skill/dynamic/mechanic/ParticleAnimationMechanic.java
+++ src/main/java/com/sucy/skill/dynamic/mechanic/ParticleAnimationMechanic.java
@@ -114,9 +114,9 @@ public class ParticleAnimationMechanic extends MechanicComponent {
             this.targets = targets;
             this.settings = settings;
 
-            this.forward = parseValues(caster, FORWARD, level, 0);
-            this.upward = parseValues(caster, UPWARD, level, 0);
-            this.right = parseValues(caster, RIGHT, level, 0);
+            this.forward = getNum(caster, FORWARD, 0);
+            this.upward = getNum(caster, UPWARD, 0);
+            this.right = getNum(caster, RIGHT, 0);
 
             this.steps = settings.getInt(STEPS, 1);
             this.freq = (int) (settings.getDouble(FREQ, 1.0) * 20);
