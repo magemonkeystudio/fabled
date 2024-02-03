645016aa18f163821d2750ea66e6bdf10f8b59fe
Travja <the.only.t.craft@gmail.com>
2024-02-03 12:19:32 -0700

    Use wss

--- src/api/socket/socket-connector.ts
+++ src/api/socket/socket-connector.ts
@@ -65,7 +65,7 @@ class SocketService {
 		this.clientId   = clientName;
 		const clientKey = Math.floor(10000 + Math.random() * 90000).toString();
 
-		this.socket = io('ws://synthesis.travja.dev', {
+		this.socket = io('wss://synthesis.travja.dev', {
 			auth: {
 				sessionId: this.sessionId,
 				clientId:  this.clientId,
