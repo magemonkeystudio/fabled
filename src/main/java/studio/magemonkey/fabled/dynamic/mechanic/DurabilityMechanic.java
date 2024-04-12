c4650407afd2691f4abaa1d7081c1e285178edfe
Travja <the.only.t.craft@gmail.com>
2024-04-11 22:03:04 -0600

    Make durability mechanic able to repair items

--- src/main/java/studio/magemonkey/fabled/dynamic/mechanic/DurabilityMechanic.java
+++ src/main/java/studio/magemonkey/fabled/dynamic/mechanic/DurabilityMechanic.java
@@ -1,10 +1,12 @@
 package studio.magemonkey.fabled.dynamic.mechanic;
 
-import studio.magemonkey.codex.mccore.util.VersionManager;
 import org.bukkit.Sound;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
+import studio.magemonkey.codex.mccore.util.VersionManager;
 
 import java.util.List;
 
@@ -13,7 +15,6 @@ import java.util.List;
  * studio.magemonkey.fabled.dynamic.mechanic.DurabilityMechanic
  */
 public class DurabilityMechanic extends MechanicComponent {
-
     private static final String AMOUNT  = "amount";
     private static final String OFFHAND = "offhand";
 
@@ -25,7 +26,6 @@ public class DurabilityMechanic extends MechanicComponent {
     @Override
     public boolean execute(
             final LivingEntity caster, final int level, final List<LivingEntity> targets, boolean force) {
-
         if (!(caster instanceof Player)) {
             return false;
         }
@@ -38,14 +38,21 @@ public class DurabilityMechanic extends MechanicComponent {
         if (offhand && VersionManager.isVersionAtLeast(VersionManager.V1_9_0)) {
             item = player.getInventory().getItemInOffHand();
         } else {
+            //noinspection deprecation
             item = player.getInventory().getItemInHand();
         }
 
-        if (item == null || item.getType().getMaxDurability() == 0) {
+        if (item.getType().isAir() || item.getType().getMaxDurability() == 0) {
             return false;
         }
 
-        int durability = item.getType().getMaxDurability() - item.getDurability();
+        ItemMeta itemMeta = item.getItemMeta();
+        if (!(itemMeta instanceof Damageable)) {
+            return false;
+        }
+
+        Damageable im         = (Damageable) itemMeta;
+        int        durability = item.getType().getMaxDurability() - im.getDamage();
         if (durability <= -amount) {
             if (offhand && VersionManager.isVersionAtLeast(VersionManager.V1_9_0)) {
                 player.getInventory().setItemInOffHand(null);
@@ -54,7 +61,8 @@ public class DurabilityMechanic extends MechanicComponent {
             }
             player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1, 1);
         }
-        item.setDurability((short) (item.getDurability() - amount));
+        im.setDamage(im.getDamage() + amount);
+        item.setItemMeta(im);
         return true;
     }
 }
