01af3f40ace7073ab31705ab22f3c517fb268fa5
RAYCAL2000 <rcal2500@gmail.com>
2024-10-15 10:58:34 -0400

    Allowing flyspeed to be changed

--- src/main/java/studio/magemonkey/fabled/dynamic/mechanic/FlyMechanic.java
+++ src/main/java/studio/magemonkey/fabled/dynamic/mechanic/FlyMechanic.java
@@ -6,11 +6,13 @@ import org.bukkit.entity.Player;
 import java.util.List;
 
 /**
- * Allows target to fly 
+ * Grants or takes away creative level flight to a player.
+ * Does not persist on logout. 
  */
 public class FlyMechanic extends MechanicComponent {
     private static final String FLYSPEED = "flyspeed";
     private static final String SECONDS = "seconds";
+
 
     @Override
     public String getKey() {
@@ -28,15 +30,23 @@ public class FlyMechanic extends MechanicComponent {
      */
     @Override
     public boolean execute(LivingEntity caster, int level, List<LivingEntity> targets, boolean force) {
-        double flyspeed = parseValues(caster, FLYSPEED, level, 0.6);
+        float flyspeed = (float) parseValues(caster, FLYSPEED, level, 0.6);
         double seconds = parseValues(caster, SECONDS, level, 3.0);
         
         for (LivingEntity target : targets) {
             if (target instanceof Player){
                 Player player = (Player) target;
+                /*
+
+                Cause realistically you're doing this tomorrow:
+                    - Allow players to set flyspeed, must be a float. (Between -1,1)
+                    - Duration, how long can fly.
+                    - Set a default fallback speed!
+
+                */ 
                 player.setAllowFlight(true);
                 player.setFlying(true);
-                player.setFlySpeed(0.06f);
+                player.setFlySpeed(flyspeed);
             }
         }
 
