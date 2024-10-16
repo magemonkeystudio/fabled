e93737d006eea1d9bcb73809a1eaf5e2fe7bf3cc
RAYCAL2000 <rcal2500@gmail.com>
2024-10-16 09:25:21 -0400

    Attempting to get a duration to work for the fly effect

--- src/main/java/studio/magemonkey/fabled/dynamic/mechanic/FlyMechanic.java
+++ src/main/java/studio/magemonkey/fabled/dynamic/mechanic/FlyMechanic.java
@@ -2,20 +2,40 @@ package studio.magemonkey.fabled.dynamic.mechanic;
 
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+import studio.magemonkey.fabled.Fabled;
+import studio.magemonkey.fabled.api.player.PlayerData;
+import studio.magemonkey.fabled.api.player.PlayerStatModifier;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Sets the flight state and flight speed of a player.
  * Does not persist on logout. 
  */
 public class FlyMechanic extends MechanicComponent {
+    private static final String SECONDS = "seconds";
     private static final String FLYSPEED = "flyspeed";
     private static final String FLYING = "flying"; 
 
+    private final Map<Integer, Map<String, FlyTask>> tasks = new HashMap<>();
 
     @Override
     public String getKey() {
         return "fly";
+    }
+    
+    @Override
+    protected void doCleanUp(final LivingEntity user) {
+        final Map<String, FlyTask> casterTasks = tasks.remove(user.getEntityId());
+        if (casterTasks != null) {
+            casterTasks.values().forEach(FlyTask::stop);
+        }
     }
 
     /**
@@ -29,27 +49,72 @@ public class FlyMechanic extends MechanicComponent {
      */
     @Override
     public boolean execute(LivingEntity caster, int level, List<LivingEntity> targets, boolean force) {
-        boolean flying = settings.getString(FLYING, "false").equalsIgnoreCase("true");
-        float flyspeed = (float) parseValues(caster, FLYSPEED, level, 0.1);       
+        final double    seconds  = parseValues(caster, SECONDS, level, 3.0);
+        final int       ticks    = (int) (seconds * 20);
+        float           flyspeed = (float) parseValues(caster, FLYSPEED, level, 0.1);       
 
         for (LivingEntity target : targets) {
             // Only target players.
             if (target instanceof Player){
                 Player player = (Player) target;
-                player.setAllowFlight(flying);
-                player.setFlying(flying);
-                if (flying){
-                    // Flightspeed cannot be greater than 1 or less than -1.
-                    if (flyspeed > 1){
-                        flyspeed = 1.0f;
-                    }
-                    else if (flyspeed < -1){
-                        flyspeed = -1.0f;
-                    }
-                    player.setFlySpeed(flyspeed);
+                final PlayerData data = Fabled.getData((Player) target);
+                player.setAllowFlight(true);
+                player.setFlying(true);
+                // Flightspeed cannot be greater than 1 or less than -1.
+                if (flyspeed > 1){
+                    flyspeed = 1.0f;
+                }
+                else if (flyspeed < -1){
+                    flyspeed = -1.0f;
+                }
+                final FlyTask task = new FlyTask(caster.getEntityId(), data);
+                player.setFlySpeed(flyspeed);
+                if (ticks >= 0){
+                    Fabled.schedule(task, ticks);
                 }
             }
         }
         return targets.size() > 0;
     }
+
+    private class FlyTask extends BukkitRunnable {
+
+        private final PlayerData         data;
+        private final PlayerStatModifier modifier;
+        private final int                id;
+        private       boolean            running = false;
+        private       boolean            stopped = false;
+
+        FlyTask(int id, PlayerData data) {
+            this.id = id;
+            this.data = data;
+            this.modifier = null;
+        }
+
+        public void stop() {
+            if (!stopped) {
+                stopped = true;
+                run();
+                if (running) {
+                    cancel();
+                }
+            }
+        }
+
+        @Override
+        public BukkitTask runTaskLater(final Plugin plugin, final long delay) {
+            running = true;
+            return super.runTaskLater(plugin, delay);
+        }
+
+        @Override
+        public void run() {
+            data.removeStatModifier(modifier.getUUID(), true);
+            if (tasks.containsKey(id)) {
+                tasks.get(id).remove(data.getPlayerName());
+            }
+            running = false;
+        }
+    }
+
 }
