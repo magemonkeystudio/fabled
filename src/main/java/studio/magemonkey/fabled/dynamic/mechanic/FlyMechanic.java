81b25f69b297effbce0502c47474bbaded41c4d6
RAYCAL2000 <64821088+RAYCAL2000@users.noreply.github.com>
2024-10-16 21:19:01 -0400

    Fly Mechanic Resolves #353 (#933)
    * Adding new Minecraft 1.21 Attribute Stats to be able to be tracked and edited by Fabled
    
    * Adding min and max stats for all attributes according to wiki pages
    
    * Added proper import to get current MC version, added a condition to only initialize the attributes if the proper version is running
    
    * Repushing changes, added new import and added a check to only initialize attributes if the proper MC version is running
    
    * Attempting to start a new fly mechanic which allows the player to fly
    
    * Testing flight mechanic
    
    * Fixing up fly mechanic so that it will register
    
    * fixing up my code
    
    * Seeing if I can adjust fly speed well
    
    * Lowering flyspeed heavily
    
    * Fixing it so default fly speed matches that of a bat
    
    * Allowing flyspeed to be changed
    
    * Finalziing flying mechanic, allows players to either set or unset the fly state of a player.
    
    * Fixing up bounds so a player cannot add a fly speed which is not supported by mojang
    
    * Finalizing flyspeed boolean
    
    * Attempting to get a duration to work for the fly effect
    
    * Fixing object
    
    * Adding remove flight task
    
    * Finalizing timer for fly mechanic
    
    * Adding comments to better explain how things should be working, allows players to set fly state, allows players to set duration, no longer cancels flight midway if a new skill is added
    
    * Forogt a to put casterTasks in the task area
    
    * Do not apply the flying skill if you are in Creative or Spectator as it will completely break creative flight
    
    * Stopping fly mechanic from absolutely breaking creative flight

---
+++ src/main/java/studio/magemonkey/fabled/dynamic/mechanic/FlyMechanic.java
@@ -0,0 +1,145 @@
+package studio.magemonkey.fabled.dynamic.mechanic;
+
+import org.bukkit.GameMode;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+import studio.magemonkey.fabled.Fabled;
+import studio.magemonkey.fabled.api.player.PlayerData;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Sets the flight state and flight speed of a player.
+ * Does not persist on logout. 
+ */
+public class FlyMechanic extends MechanicComponent {
+    private static final String SECONDS = "seconds";
+    private static final String FLYSPEED = "flyspeed";
+    private static final String FLYING = "flying";
+
+    private final Map<Integer, Map<String, FlyTask>> tasks = new HashMap<>();
+
+    @Override
+    public String getKey() {
+        return "fly";
+    }
+    
+    @Override
+    protected void doCleanUp(final LivingEntity user) {
+        final Map<String, FlyTask> casterTasks = tasks.remove(user.getEntityId());
+        if (casterTasks != null) {
+            casterTasks.values().forEach(FlyTask::stop);
+        }
+    }
+
+    /**
+     * Executes the component
+     * 
+     * @param caster  caster of the skill
+     * @param level   level of the skill
+     * @param targets targets to apply to
+     * @param force
+     * @return true if applied to something, false otherwise
+     */
+    @Override
+    public boolean execute(LivingEntity caster, int level, List<LivingEntity> targets, boolean force) {
+        final double    seconds  = parseValues(caster, SECONDS, level, 3.0); // Get seconds or default to 3 seconds.
+        final int       ticks    = (int) (seconds * 20);
+        float           flyspeed = (float) parseValues(caster, FLYSPEED, level, 0.1); // Get flyspeed or default value.
+        boolean         flying   = settings.getString(FLYING, "false").equalsIgnoreCase("true"); // Get if a player wants to grant or remove flight.
+        final Map<String, FlyTask> casterTasks = tasks.computeIfAbsent(caster.getEntityId(), HashMap::new); // Map of all current Tasks.
+
+        for (LivingEntity target : targets) {
+            // Only target players.
+            if (target instanceof Player){
+                Player player = (Player) target;
+                // Do not set flying to false if player is in spectator or creative.
+                if (player.getGameMode() == GameMode.SURVIVAL || player.getGameMode() == GameMode.ADVENTURE) {
+                    final PlayerData data = Fabled.getData((Player) target);
+                    // Bound Flightspeed as it cannot be greater than 1 or less than -1.
+                    if (flyspeed > 1){
+                        flyspeed = 1.0f;
+                    }
+                    else if (flyspeed < -1){
+                        flyspeed = -1.0f;
+                    }
+                    // Set player flight based on given boolean.
+                    player.setAllowFlight(flying);
+                    player.setFlying(flying);
+                    player.setFlySpeed(flyspeed);
+
+                    /* 
+                    / Cancel previous tasks if one already exists.
+                    / This allows flight to be extended if players cast multiple skills.
+                    / Without this players may fall too early or unexpectedly.
+                    */
+                    if (casterTasks.containsKey(data.getPlayerName())){
+                        final FlyTask oldTask = casterTasks.remove(data.getPlayerName());
+                        oldTask.cancel();
+                    }
+                    // Only create a new task and schedule if the players wants flight, otherwise do nothing.
+                    if (flying) {
+                        final FlyTask task = new FlyTask(caster.getEntityId(), data);
+                        casterTasks.put(data.getPlayerName(), task);
+                        if (ticks >= 0){
+                            Fabled.schedule(task, ticks);
+                        }
+                    }
+                }
+            }
+        }
+        return targets.size() > 0;
+    }
+
+    private class FlyTask extends BukkitRunnable {
+
+        private final PlayerData         data;
+        private final int                id;
+        private       boolean            running = false;
+        private       boolean            stopped = false;
+
+        FlyTask(int id, PlayerData data) {
+            this.id = id;
+            this.data = data;
+        }
+
+        public void stop() {
+            if (!stopped) {
+                stopped = true;
+                run();
+                if (running) {
+                    cancel();
+                }
+            }
+        }
+
+        @Override
+        public BukkitTask runTaskLater(final Plugin plugin, final long delay) {
+            running = true;
+            return super.runTaskLater(plugin, delay);
+        }
+
+        @Override
+        public void run() {
+            Player player = data.getPlayer();
+            // Do not set flying to false if player is in spectator or creative.
+            if (player.getGameMode() == GameMode.SURVIVAL || player.getGameMode() == GameMode.ADVENTURE){
+                player.setFlying(false);
+                player.setAllowFlight(false);
+            }
+            // Set player back to default fly speed.
+            player.setFlySpeed(0.1f);
+            if (tasks.containsKey(id)) {
+                tasks.get(id).remove(data.getPlayerName());
+            }
+            running = false;
+        }
+    }
+
+}
