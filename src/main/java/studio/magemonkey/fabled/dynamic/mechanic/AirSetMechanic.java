5d185c82cdbeb26abe236fa0d6ba9f6bc6d56913
RAYCAL2000 <rcal2500@gmail.com>
2024-11-01 10:21:33 -0400

    Adding in brand new Set Air mechanic

---
+++ src/main/java/studio/magemonkey/fabled/dynamic/mechanic/AirSetMechanic.java
@@ -0,0 +1,30 @@
+package studio.magemonkey.fabled.dynamic.mechanic;
+
+import org.bukkit.entity.LivingEntity;
+
+import java.util.List;
+
+/**
+ * Fabled © 2024
+ * studio.magemonkey.fabled.dynamic.mechanic.HealthSetMechanic
+ */
+public class AirSetMechanic extends MechanicComponent {
+
+    private static final String AIR = "air";
+
+    @Override
+    public String getKey() {
+        return "air set";
+    }
+
+    @Override
+    public boolean execute(final LivingEntity caster, final int level, final List<LivingEntity> targets, boolean force) {
+        final int air = Math.max(1, (int) parseValues(caster, AIR, level, 1));
+
+        for (final LivingEntity target : targets) {
+            target.setHealth(Math.min(air, target.getMaximumAir()));
+        }
+
+        return true;
+    }
+}
