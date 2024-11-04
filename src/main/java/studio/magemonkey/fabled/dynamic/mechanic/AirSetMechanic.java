7d3ca720547cfc6ece1744752698dab1a34ad2a8
RAYCAL2000 <rcal2500@gmail.com>
2024-11-04 10:55:10 -0500

    Making it so that we cannot go above the maximum air of a target

--- src/main/java/studio/magemonkey/fabled/dynamic/mechanic/AirSetMechanic.java
+++ src/main/java/studio/magemonkey/fabled/dynamic/mechanic/AirSetMechanic.java
@@ -20,9 +20,12 @@ public class AirSetMechanic extends MechanicComponent {
     @Override
     public boolean execute(final LivingEntity caster, final int level, final List<LivingEntity> targets, boolean force) {
         final double air = Math.max(1, parseValues(caster, AIR, level, 1));
-        final int ticks = (int) (air * 20);
+        int ticks = (int) (air * 20);
 
         for (final LivingEntity target : targets) {
+            if (ticks > target.getMaximumAir()){
+                ticks = target.getMaximumAir();
+            }
             target.setRemainingAir(ticks);
         }
 
