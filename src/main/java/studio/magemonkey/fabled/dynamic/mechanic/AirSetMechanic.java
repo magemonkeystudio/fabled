6076a39baaafa7fef1f72d8f96ac058334f0c462
RAYCAL2000 <rcal2500@gmail.com>
2024-11-04 12:29:32 -0500

    Removing the max value check to allow for air to be negative

--- src/main/java/studio/magemonkey/fabled/dynamic/mechanic/AirSetMechanic.java
+++ src/main/java/studio/magemonkey/fabled/dynamic/mechanic/AirSetMechanic.java
@@ -19,7 +19,7 @@ public class AirSetMechanic extends MechanicComponent {
 
     @Override
     public boolean execute(final LivingEntity caster, final int level, final List<LivingEntity> targets, boolean force) {
-        final double air = Math.max(1, parseValues(caster, AIR, level, 1));
+        final double air = parseValues(caster, AIR, level, 1);
         int ticks = (int) (air * 20);
 
         for (final LivingEntity target : targets) {
