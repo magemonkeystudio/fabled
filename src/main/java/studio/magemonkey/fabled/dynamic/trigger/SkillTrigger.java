07a52233657b447135db27af32b68cabf65712c0
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2026-01-07 02:02:59 +0800

    added `api-damage` value in Skill Damage Trigger

--- src/main/java/studio/magemonkey/fabled/dynamic/trigger/SkillTrigger.java
+++ src/main/java/studio/magemonkey/fabled/dynamic/trigger/SkillTrigger.java
@@ -1,5 +1,7 @@
 package studio.magemonkey.fabled.dynamic.trigger;
 
+import studio.magemonkey.fabled.Fabled;
+import studio.magemonkey.fabled.api.CastData;
 import studio.magemonkey.fabled.api.Settings;
 import studio.magemonkey.fabled.api.event.SkillDamageEvent;
 import studio.magemonkey.fabled.dynamic.DynamicSkill;
@@ -28,9 +30,15 @@ public abstract class SkillTrigger implements Trigger<SkillDamageEvent> {
         final double       min   = settings.getDouble("dmg-min");
         final double       max   = settings.getDouble("dmg-max");
         final List<String> types = settings.getStringList("category");
-        final boolean      empty = types.isEmpty() || types.get(0).isEmpty();
+        final boolean empty = types.isEmpty() || types.get(0).isEmpty() || types.get(0).equals("default");
+        if (!empty) Fabled.inst().getLogger().warning("Skill damage trigger category is not empty, values: " + types);
         return event.getDamage() >= min && event.getDamage() <= max &&
                 (empty || types.contains("default") || types.contains(event.getClassification()));
+    }
+
+    @Override
+    public void setValues(SkillDamageEvent event, CastData data) {
+        data.put("api-damage", event.getDamage());
     }
 
     /**
