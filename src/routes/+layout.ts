fcdf1c023afd5b4170e55627baa7baaa7a99dec4
Trav <the.only.t.craft@gmail.com>
2026-05-11 19:11:42 -0600

    Hydrate before socket connection

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,13 +1,15 @@
-import type { LayoutLoad } from './$types';
-import { socketService } from '$api/socket/socket-connector';
-import { initComponents } from '$api/components/components.svelte';
-import { synthesisEnabled } from '../data/settings';
+import type { LayoutLoad }   from './$types';
+import { socketService }     from '$api/socket/socket-connector';
+import { initComponents }    from '$api/components/components.svelte';
+import { synthesisEnabled }  from '../data/settings';
 import { hydrateEditorData } from '../data/editor-session';
 
 export const ssr = false;
 
 export const load: LayoutLoad = async ({ url }) => {
 	initComponents();
+	await hydrateEditorData();
+
 	if (synthesisEnabled && url.searchParams.has('session')) {
 		// Attempt to connect to the socket.io server
 		const sessionId = url.searchParams.get('session');
@@ -15,6 +17,4 @@ export const load: LayoutLoad = async ({ url }) => {
 			socketService.connect(sessionId);
 		}
 	}
-
-	await hydrateEditorData();
 };
